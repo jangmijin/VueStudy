efcf2f1017d9143634dd2b2f3df30f5cfab9fc6a
jangmijin <tlrud0621@gmail.com>
2018-02-23 17:20:34 +0900

    Vue single-file-component

---
+++ 9_vue_single_file_component/quiz_single-file-components/src/components/LocalComponent.vue
@@ -0,0 +1,5 @@
+<template lang="html">
+	<div class="">
+		<h5>Local component</h5>
+	</div>
+</template>
---
+++ 9_vue_single_file_component/quiz_single-file-components/src/main.js
@@ -0,0 +1,15 @@
+import Vue from 'vue'
+import App from './App.vue'
+// Global Components
+import NavHeader from './components/NavHeader.vue';
+import GlobalComponent from './components/GlobalComponent.vue';
+
+Vue.component('nav-header', NavHeader);
+// 할일 #2
+// Global 컴포넌트를 하나 더 등록하고 nav-header 아래에 렌더링 해보세요.
+Vue.component('global-component', GlobalComponent);
+
+new Vue({
+  el: '#app',
+  render: h => h(App)
+})
